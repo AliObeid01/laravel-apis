e797433f8339e4f4d8872760adad59f34a4a2a24
unknown <aliiobeid01@gmail.com>
2022-09-30 22:01:34 +0300

    backend:add the route of the function sortString

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\api_controller;
 Route::get("/placeValue-api/{num?}",[api_controller::class, 'placeValue']);
 Route::get("/evaluateExpr-api/{string?}",[api_controller::class, 'evaluateExpr']);
 Route::get("/binary-api/{string?}",[api_controller::class, 'binary']);
+Route::get("/sortString-api/{string?}",[api_controller::class, 'sortString']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
