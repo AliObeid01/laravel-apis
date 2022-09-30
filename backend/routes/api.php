9e6483502aee73168e76cb84cb3f7f4ec0f6658e
unknown <aliiobeid01@gmail.com>
2022-09-30 23:03:57 +0300

    backend:add the route of the function binary

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\api_controller;
 
 Route::get("/placeValue-api/{num?}",[api_controller::class, 'placeValue']);
 Route::get("/evaluateExpr-api/{string?}",[api_controller::class, 'evaluateExpr']);
-
+Route::get("/binary-api/{string?}",[api_controller::class, 'binary']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
