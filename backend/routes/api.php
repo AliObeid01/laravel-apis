d54546ec997cb639cad30184da9768501556458c
unknown <aliiobeid01@gmail.com>
2022-09-30 22:04:11 +0300

    backend:add group route v1 for all the apis

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,11 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api_controller;
 
+Route::group(["prefix"=>"v1"],function(){
 
-Route::get("/placeValue-api/{num?}",[api_controller::class, 'placeValue']);
-Route::get("/evaluateExpr-api/{string?}",[api_controller::class, 'evaluateExpr']);
-Route::get("/binary-api/{string?}",[api_controller::class, 'binary']);
-Route::get("/sortString-api/{string?}",[api_controller::class, 'sortString']);
+    Route::get("/placeValue-api/{num?}",[api_controller::class, 'placeValue']);
+    Route::get("/evaluateExpr-api/{string?}",[api_controller::class, 'evaluateExpr']);
+    Route::get("/binary-api/{string?}",[api_controller::class, 'binary']);
+    Route::get("/sortString-api/{string?}",[api_controller::class, 'sortString']);
+
+ });
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
